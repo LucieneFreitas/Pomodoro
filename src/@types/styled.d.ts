46b205e408fd0470e1ec4d71ac9ea13f7a1d3027
Luciene Freitas <luciene_freitas@yahoo.com.br>
2024-09-03 17:06:07 -0300

    paginas e rotas
    criada paginas e rotas

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -1,8 +1,16 @@
 import 'styled-components'
 import { defaultTheme } from '../styles/themes/default'
 
+// Define um novo tipo `ThemeType` que corresponde ao tipo do objeto `defaultTheme`.
+// Isso permite que o TypeScript entenda quais propriedades e valores estão disponíveis no tema.
 type ThemeType = typeof defaultTheme
 
+// Extende o módulo 'styled-components' com uma nova interface `DefaultTheme`.
+// Isso permite que qualquer componente estilizado com `styled-components` acesse o tema tipado, 
+// garantindo que as propriedades do tema sejam verificadas pelo TypeScript.
 declare module 'styled-components' {
   export interface DefaultTheme extends ThemeType {}
+// A interface `DefaultTheme` agora herda todas as propriedades e tipos definidos em `ThemeType`.
 }
+
+
